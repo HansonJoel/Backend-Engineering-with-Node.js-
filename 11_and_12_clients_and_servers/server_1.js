952d170e027919f15976b0cdad2e0ed7538dcf9a
HansonJoel <joelhanson002@gmail.com>
2025-07-09 17:32:03 +0100

    label the server handling method

--- 11_and_12_clients_and_servers/server_1.js
+++ 11_and_12_clients_and_servers/server_1.js
@@ -3,7 +3,8 @@ const http = require("http");
 const HOSTNAME = "localhost";
 const PORT = 8000;
 
-function requestHandler(req, res) {
+// Asynchronous server handling
+function requestHandler(req, res) {     
   // console.log(req)
   console.log("incoming request received");
   res.writeHead(200, { "Content-Type": "text/plain" });
--- 11_and_12_clients_and_servers/server_2.js
+++ 11_and_12_clients_and_servers/server_2.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
-const server = http.createServer(function (req, res) {
+// Synchronous server handling
+const server = http.createServer(function (req, res) {    
   res.writeHead(200, { "Content-Type": "text/plain" });
   console.log("Incoming request received");
   res.write(`Hello there, this is my response \n`);
